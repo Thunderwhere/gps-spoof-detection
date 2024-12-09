b7bc259b7b807bfef0638210ffff7f7107087be9
Thunderwhere <brandonrshady@gmail.com>
2024-12-08 21:04:06 -0500

    snr_analysis.m fix

--- individual-checks/snr_analysis.m
+++ individual-checks/snr_analysis.m
@@ -3,6 +3,9 @@ filePath = 'gnss_log_2024_10_27_11_47_14.nmea'; % Update with your file name
 
 % Open the file and read the contents
 fileID = fopen(filePath, 'r');
+if fileID == -1
+    error('Failed to open file: %s', filePath);
+end
 rawData = textscan(fileID, '%s', 'Delimiter', '\n'); % Read all lines
 fclose(fileID);
 rawData = rawData{1}; % Store the lines in a cell array
